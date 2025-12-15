6f07e8dd6f63764b0477e73c81b833a9a67bebf1
felix <duemarsgh@foxmail.com>
2025-12-15 21:51:20 +0800

    feat(config): 在 options 中添加自定义配置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 设置 tab 缩进宽度为 4 空格
+vim.opt.tabstop = 4
+-- 不设置自动缩进宽度
+vim.opt.shiftwidth = 0
+
+-- 始终显示符号列(用于显示 error, git 等诊断信息)
+vim.opt.signcolumn = "yes"
