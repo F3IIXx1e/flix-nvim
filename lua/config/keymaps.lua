533632a5580159b426b467ffdc86f89657ef3b52
felix <duemarsgh@foxmail.com>
2025-12-23 21:18:56 +0800

    feat(config): 新增快捷键绑定
    - 通过自定义 map 函数统一注册快捷键
    - 获取 LazyVim 默认的 leader/localleader 键
    - 设置 Ctrl+z 为撤销快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 通过 map 函数注册快捷键
+local function map(mode, lhs, rhs, opts)
+    local options = { silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- 获取 leader 键
+local leader = vim.g.mapleader
+local localleader = vim.g.maplocalleader
+
+-- 设置 Ctrl+z 为撤销
+map({ "n", "i", "v" }, "<C-z>", "<esc><cmd>undo<cr>", { desc = "undo" })
